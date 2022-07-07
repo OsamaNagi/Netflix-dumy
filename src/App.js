07089733f72d8d5361b83de7696f2537792b9c99
OsamaNagi <alshahidyosama@gmail.com>
2022-07-07 19:30:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,43 +8,43 @@ import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "./features/userSlice";
 import ProfileScreen from "./screens/ProfileScreen";
 
-function App() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
-      if (userAuth) {
-        console.log(userAuth);
-        dispatch(
-          login({
-            userID: userAuth.userID,
-            email: userAuth.email,
-          })
-        );
-      } else {
-        dispatch(logout());
-      }
-    });
-    return unsubscribe;
-  }, [dispatch]);
-  return (
-    <div className="app">
-      <Router>
-        {!user ? (
-          <LoginScreen />
-        ) : (
-          <Fragment>
-            <Routes>
-              <Route path="/profile" element={<ProfileScreen />} />
-            </Routes>
-            <Routes>
-              <Route exact path="/" element={<HomeScreen />} />
-            </Routes>
-          </Fragment>
-        )}
-      </Router>
-    </div>
-  );
+function App () {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                console.log(userAuth);
+                dispatch(
+                    login({
+                        userID: userAuth.userID,
+                        email: userAuth.email,
+                    })
+                );
+            } else {
+                dispatch(logout());
+            }
+        });
+        return unsubscribe;
+    }, [ dispatch ]);
+    return (
+        <div className="app">
+            <Router>
+                { !user ? (
+                    <LoginScreen />
+                ) : (
+                    <Fragment>
+                        <Routes>
+                            <Route path="/profile" element={ <ProfileScreen /> } />
+                        </Routes>
+                        <Routes>
+                            <Route exact path="/" element={ <HomeScreen /> } />
+                        </Routes>
+                    </Fragment>
+                ) }
+            </Router>
+        </div>
+    );
 }
 
 export default App;
